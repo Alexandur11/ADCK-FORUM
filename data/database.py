ea9428dec10096b1afa0596a9344a442d2066bcb
AlexandurDaskalov <AlexandurDaskalov11@gmail.com>
2024-05-13 21:42:05 +0300

    Docker

--- data/database.py
+++ data/database.py
@@ -1,12 +1,12 @@
-import mysql.connector
-from creditentials import *
-mydb = mysql.connector.connect(
-    host=f"{host}",
-    user=f"{user}",
-    password=f"{password}",
-    port=f"{port}",
-    database=f"{database}"
-)
+# import mysql.connector
+# from creditentials import *
+# mydb = mysql.connector.connect(
+#     host=f"{host}",
+#     user=f"{user}",
+#     password=f"{password}",
+#     port=f"{port}",
+#     database=f"{database}"
+# )
 
 
 def read_query(sql: str, sql_params=()):
